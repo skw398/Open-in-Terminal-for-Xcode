92f345e1b3aebb0b5c3990db678098c8d86b403c
oshio <skw398@gmail.com>
2025-04-28 22:48:11 +0900

    init

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p "$HOME/Library/Services"
+cp -R Open_in_Terminal.workflow "$HOME/Library/Services/"
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf "$HOME/Library/Services/Open_in_Terminal.workflow"
